9f80ec9791fb116c4194ca97eb91f350b70b8139
adamjacobbecker <ad@mbecker.cc>
2013-01-16 14:19:02 -0500

    "realistic" seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,21 @@
 
 require 'ffaker'
 
+checkin_body = <<END
+##Get Done
+repair banana stand
+find company checkbook
+
+##Got Done
+rocked karaoke with maeby
+
+##Flags
+Y: refrigerator keeps falling through the wall
+
+##Shelf
+get mother into a home
+END
+
 User.create(name: "Adam Becker", email: "ad@mbecker.cc", password: "password")
 User.create(name: "Tobias Funke", email: "tobias@gobiasindustries.com", password: "password")
 User.create(name: "Michael Bluth", email: "michael@bluthco.com", password: "password")
@@ -18,7 +33,7 @@ User.all.each do |user|
 
   10.times do
     break if rand(1..5) == 1
-    checkin = user.checkins.build body: Faker::Lorem.sentence
+    checkin = user.checkins.build body: checkin_body
     checkin.created_at = date.to_time
     checkin.save
     date = date - 1.day
