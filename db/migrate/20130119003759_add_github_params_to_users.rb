1e6a529ad05dbe0f4617cb0bba53b51030d38bf1
adamjacobbecker <ad@mbecker.cc>
2013-01-18 20:32:39 -0500

    fix migrate down

--- db/migrate/20130119003759_add_github_params_to_users.rb
+++ db/migrate/20130119003759_add_github_params_to_users.rb
@@ -2,6 +2,6 @@ class AddGithubParamsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :login, :string
     add_column :users, :access_token, :string
-    remove_columns :users, :password
+    remove_columns :users, :password_digest
   end
 end
