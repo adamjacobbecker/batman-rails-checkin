e39c5064ac99d18f4a9f100ccf8e598855e76450
adamjacobbecker <ad@mbecker.cc>
2013-01-24 09:24:39 -0500

    use gmail to send email

---
+++ config/initializers/gmail.rb
@@ -0,0 +1,10 @@
+require 'smtp_tls'
+
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :authentication => :plain,
+  :domain => ENV['GMAIL_SMTP_USER'],
+  :user_name => ENV['GMAIL_SMTP_USER'],
+  :password => ENV['GMAIL_SMTP_PASSWORD'],
+}
